9c2f8e700e2057bf635df54f3b875314ab3e7792
teunbuijse <teunbuijse@gmail.com>
2020-03-08 23:56:12 +0100

    gui.py verbeterd met punten en hints knop

--- gui.py
+++ gui.py
@@ -1,18 +1,20 @@
 from tkinter import *
+from random import *
 import random
 import string
+
 
 root = Tk()
 root.geometry("900x600")
 root.configure(background = 'lightgray')
 root.title("SpellingBee")
 
-entry1=Entry(root, width = 10)
-entry1.place(x=160,y=470)
-entry1.configure(highlightbackground = 'lightgray', background = 'lightgray')
+entry1=Entry(root)
+entry1.place(x=130,y=475, width = 115, height = 30)
+entry1.configure(highlightbackground = 'white', background = 'white')
 
 def four_or_longer():
-	with open('alle_nederlandse_woorden', encoding="utf-8") as f:
+	with open('woordenlijst.txt', encoding="utf-8") as f:
 		f = [line.rstrip('\n') for line in f]
 		listing = []
 	for x in f:
@@ -23,7 +25,7 @@ def four_or_longer():
 input = four_or_longer()
 
 def random_word_picker():
-	with open('alle_nederlandse_woorden', encoding="utf-8") as f:
+	with open('woordenlijst.txt', encoding="utf-8") as f:
 		f = [line.rstrip('\n') for line in f]
 		ties = [word for word in f if len(word) == 7]
 		my_list = []
@@ -71,12 +73,42 @@ print (list_valid_words1)
 def save_input():
 	woordinput = entry1.get()
 	if woordinput in list_valid_words1:
+		list_valid_words1.pop(list_valid_words1.index(woordinput))
+		global score
+
+				
+	#	if len(woordinput) == 7:
+	#		for index, letter in enumerate(woordinput):
+	#			if letter in woordinput[index:]:
+	#				pass
+	#				score += 7
+		score += len(woordinput) - 3
 		label2 = Label(root, text= woordinput)
 		label2.configure(width=10, background = 'lightgray', font=("Courier", 30))
 		label2.place(x=600, y=90)
+		label3 = Label(root, text= 'Punten: {}'.format(score))
+		label3.configure(width=20, background = 'green', font=("Courier", 15))
+		label3.place(x=600, y=490)
+		label3 = Label(root, text= 'Woorden: {}'.format(len(list_valid_words1)))
+		label3.configure(width=20, background = 'lightgray', font=("Courier", 15))
+		label3.place(x=600, y=450)
 	entry1.delete(0, END)
 	return (woordinput)
+
+
+def give_hint():
+	global score
+	score -= 1
+	hint = ''
+	hint_word = list_valid_words1[randrange(0,len(list_valid_words1))]
+	for letter in range(len(hint_word)):
+		if letter % 2 == 0:
+			hint += hint_word[letter]
+		else:
+			hint += '.'
+		
 	
+	print(hint)
 	
 
 def klik1():
@@ -100,14 +132,17 @@ def klik6():
 def klik7():
 	entry1.insert('end',random_list[6])
 
+score = 0
 button1 = Button(root, text= random_list[0], command=klik1)
 button2 = Button(root, text= random_list[1], command=klik2)
 button3 = Button(root, text= random_list[2], command=klik3)
-button4 = Button(root, text= random_list[3], command=klik4, highlightbackground = '#FEE12B')
+button4 = Button(root, text= random_list[3], command=klik4, highlightbackground = '#FEE12B', bg = '#FEE12B')
 button5 = Button(root, text= random_list[4], command=klik5)
 button6 = Button(root, text= random_list[5], command=klik6)
 button7 = Button(root, text= random_list[6], command=klik7)
-submitbutton = Button(root, text = 'Submit', height = 1, width = 10, command = save_input, highlightbackground = '#FEE12B')
+submitbutton = Button(root, text = 'Submit', height = 1, width = 10, command= save_input, highlightbackground = 'green', bg = 'green')
+hintbutton = Button(root, text = 'Hint?', height = 1, width = 10, command= give_hint, highlightbackground = '#FEE12B', bg = '#FEE12B')
+
 
 button1.place(x=80, y=90, width=115, height=115)
 button1.configure(font=("Courier", 20, "bold"))
@@ -124,5 +159,6 @@ button6.configure(font=("Courier", 20, "bold"))
 button7.place(x=320, y=210, width=115, height=115)
 button7.configure(font=("Courier", 20, "bold"))
 submitbutton.place(x=270, y=475)
+hintbutton.place(x=400, y=475)
 
 root.mainloop()
